815ebb9b4de471711c6ab48828c88b2c4382ff51
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-25 21:14:42 +0900

    feat: add Minus icon (generated by UDS Code Generator)

---
+++ packages/ui/src/components/icons/Minus.tsx
@@ -0,0 +1,23 @@
+import { memo } from "react";
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  color?: string;
+}
+
+const MinusImpl = ({ size = 24, color = "currentColor", ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2ZM7 11C6.44772 11 6 11.4477 6 12C6 12.5523 6.44772 13 7 13H17C17.5523 13 18 12.5523 18 12C18 11.4477 17.5523 11 17 11H7Z" fill={color}/>
+  </svg>
+);
+
+const Minus = memo(MinusImpl);
+Minus.displayName = "Minus";
+export { Minus };
