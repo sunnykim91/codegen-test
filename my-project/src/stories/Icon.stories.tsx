cea4594b220fbb8f7f14c21fa8f8ed9f647c0caf
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-25 21:20:26 +0900

    feat: add Icon story (generated by UDS Code Generator)

---
+++ my-project/src/stories/Icon.stories.tsx
@@ -0,0 +1,58 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Icon } from "../components/Icon";
+
+const meta: Meta<typeof Icon> = {
+  title: "UI/Icon",
+  component: Icon,
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      control: "select",
+      options: ["14", "16", "18", "20", "22", "24", "28", "32", "40"],
+    }
+  },
+  args: {
+    size: "14"
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Icon>;
+
+export const Default: Story = {};
+
+export const 14: Story = {
+  args: { size: "14" },
+};
+
+export const 16: Story = {
+  args: { size: "16" },
+};
+
+export const 18: Story = {
+  args: { size: "18" },
+};
+
+export const 20: Story = {
+  args: { size: "20" },
+};
+
+export const 22: Story = {
+  args: { size: "22" },
+};
+
+export const 24: Story = {
+  args: { size: "24" },
+};
+
+export const 28: Story = {
+  args: { size: "28" },
+};
+
+export const 32: Story = {
+  args: { size: "32" },
+};
+
+export const 40: Story = {
+  args: { size: "40" },
+};
