618a48166edf1f2cf673e0183b6c4c3b9f725cb4
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-27 11:12:12 +0900

    feat: add PlayFill icon (generated by UDS Code Generator)

---
+++ packages/ui/src/components/icons/PlayFill.tsx
@@ -0,0 +1,23 @@
+import { memo } from "react";
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+  color?: string;
+}
+
+const PlayFillImpl = ({ size = 24, color = "currentColor", ...props }: IconProps) => (
+  <svg
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M7.75194 5.43872L18.2596 11.5682C18.4981 11.7073 18.5787 12.0135 18.4396 12.252C18.3961 12.3265 18.3341 12.3885 18.2596 12.432L7.75194 18.5615C7.51341 18.7006 7.20725 18.62 7.06811 18.3815C7.0235 18.305 7 18.2181 7 18.1296V5.87061C7 5.59446 7.22386 5.37061 7.5 5.37061C7.58853 5.37061 7.67547 5.39411 7.75194 5.43872Z" fill={color}/>
+  </svg>
+);
+
+const PlayFill = memo(PlayFillImpl);
+PlayFill.displayName = "PlayFill";
+export { PlayFill };
