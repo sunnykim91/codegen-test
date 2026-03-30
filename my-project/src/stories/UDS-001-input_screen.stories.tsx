d52066b77a47e401013b88498def0b77bf79b04e
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-30 17:09:05 +0900

    feat: add UDS-001-input_screen story (generated by UDS Code Generator)

---
+++ my-project/src/stories/UDS-001-input_screen.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { UDS001InputScreen } from "../components/UDS001InputScreen";
+
+const meta: Meta<typeof UDS001InputScreen> = {
+  title: "UI/UDS001InputScreen",
+  component: UDS001InputScreen,
+  tags: ["autodocs"],
+  argTypes: {
+    title: { control: "text" },
+    description: { control: "text" },
+    buttonLabel: { control: "text" }
+  },
+  args: {
+    children: "UDS001InputScreen",
+
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof UDS001InputScreen>;
+
+export const Default: Story = {};
