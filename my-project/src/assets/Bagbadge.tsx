31aa61ac819ea8cdb9e6e489194988d920e90318
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-31 21:44:38 +0900

    feat: add Bagbadge image component (generated by UDS Code Generator)

---
+++ my-project/src/assets/Bagbadge.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react";
+import bagbadge3x from "my-project/src/assets/bagbadge@3x.png";
+
+export interface BagbadgeProps {
+  size?: number;
+  width?: number;
+  height?: number;
+  alt?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const BagbadgeComponent = ({
+  size,
+  width = 320,
+  height = 320,
+  alt = "Bag&Badge",
+  className,
+  style,
+}: BagbadgeProps) => {
+  const w = size ?? width;
+  const h = size ? Math.round(size * (320 / 320)) : height;
+
+  return (
+    <img
+      src={bagbadge3x}
+      width={w}
+      height={h}
+      alt={alt}
+      className={className}
+      style={{ ...style, objectFit: "contain" }}
+    />
+  );
+};
+
+const Bagbadge = memo(BagbadgeComponent);
+Bagbadge.displayName = "Bagbadge";
+
+export { Bagbadge };
