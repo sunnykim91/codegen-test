663a3001fde9dc224b6154d500a99733c3faa57d
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-31 21:44:25 +0900

    feat: add Moneyhand image component (generated by UDS Code Generator)

---
+++ my-project/src/assets/Moneyhand.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react";
+import moneyhand3x from "my-project/src/assets/moneyhand@3x.png";
+
+export interface MoneyhandProps {
+  size?: number;
+  width?: number;
+  height?: number;
+  alt?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const MoneyhandComponent = ({
+  size,
+  width = 320,
+  height = 320,
+  alt = "money&Hand",
+  className,
+  style,
+}: MoneyhandProps) => {
+  const w = size ?? width;
+  const h = size ? Math.round(size * (320 / 320)) : height;
+
+  return (
+    <img
+      src={moneyhand3x}
+      width={w}
+      height={h}
+      alt={alt}
+      className={className}
+      style={{ ...style, objectFit: "contain" }}
+    />
+  );
+};
+
+const Moneyhand = memo(MoneyhandComponent);
+Moneyhand.displayName = "Moneyhand";
+
+export { Moneyhand };
