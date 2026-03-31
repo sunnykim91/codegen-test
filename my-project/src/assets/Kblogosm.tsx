e1fda757d840ba064b6e806920726f7590dcd3e6
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-03-31 21:44:13 +0900

    feat: add Kblogosm image component (generated by UDS Code Generator)

---
+++ my-project/src/assets/Kblogosm.tsx
@@ -0,0 +1,39 @@
+import React, { memo } from "react";
+import kblogosm3x from "my-project/src/assets/kblogosm@3x.png";
+
+export interface KblogosmProps {
+  size?: number;
+  width?: number;
+  height?: number;
+  alt?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const KblogosmComponent = ({
+  size,
+  width = 20,
+  height = 20,
+  alt = "KBLogoSM",
+  className,
+  style,
+}: KblogosmProps) => {
+  const w = size ?? width;
+  const h = size ? Math.round(size * (20 / 20)) : height;
+
+  return (
+    <img
+      src={kblogosm3x}
+      width={w}
+      height={h}
+      alt={alt}
+      className={className}
+      style={{ ...style, objectFit: "contain" }}
+    />
+  );
+};
+
+const Kblogosm = memo(KblogosmComponent);
+Kblogosm.displayName = "Kblogosm";
+
+export { Kblogosm };
