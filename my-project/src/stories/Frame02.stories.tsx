dfa90f5de99738875d833775cf25917fb7d065f4
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-04-07 10:18:01 +0900

    feat: add Frame02 story (generated by UDS Code Generator)

---
+++ my-project/src/stories/Frame02.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Frame02 } from "../components/Frame02";
+
+const meta: Meta<typeof Frame02> = {
+  title: "UI/Frame02",
+  component: Frame02,
+  tags: ["autodocs"],
+  argTypes: {
+
+  },
+  args: {
+
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Frame02>;
+
+export const Default: Story = {};
