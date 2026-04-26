0dc8c853fb7085cb99de106aa3c75cfa38c6afd4
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-04-26 22:31:24 +0900

    feat: add IconSlot component (generated by UDS Code Generator)

---
+++ my-project/src/app/components/IconSlot.tsx
@@ -0,0 +1,44 @@
+import React, { memo, ReactNode } from "react";
+
+export type IconSlotSize = 14 | 16 | 18 | 20 | 22 | 24 | 28 | 30 | 32 | 40;
+
+export interface IconSlotProps {
+  children?: ReactNode;
+  size?: IconSlotSize;
+  color?: string;
+  className?: string;
+  style?: React.CSSProperties;
+}
+
+const IconSlotImpl = ({
+  children,
+  size = 24,
+  color = "currentColor",
+  className,
+  style,
+}: IconSlotProps) => {
+  const cloned = children
+    ? React.cloneElement(children as React.ReactElement<any>, { size, color })
+    : null;
+
+  return (
+    <span
+      className={className}
+      style={{
+        display: "inline-flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: size,
+        height: size,
+        flexShrink: 0,
+        ...style,
+      }}
+    >
+      {cloned}
+    </span>
+  );
+};
+
+const IconSlot = memo(IconSlotImpl);
+IconSlot.displayName = "IconSlot";
+export { IconSlot };
