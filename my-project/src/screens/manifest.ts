ca5908a6df51e718ad4b5e82891036792062bd65
sunnykim91 <48176511+sunnykim91@users.noreply.github.com>
2026-04-27 17:46:28 +0900

    feat: add UDSTT01 to manifest (generated by UDS Code Generator)

---
+++ my-project/src/screens/manifest.ts
@@ -0,0 +1,29 @@
+import { ComponentType } from "react";
+
+export interface ScreenMeta {
+  id: string;
+  name: string;
+  category: {
+    major: string;
+    middle: string;
+    minor?: string;
+  };
+}
+
+export const screens: ScreenMeta[] = [
+  {
+    id: "u-d-s-t-t01",
+    name: "UDS_TT_01",
+    category: {
+      major: "",
+      middle: "",
+    },
+  },
+];
+
+export const screenImports: Record<
+  string,
+  () => Promise<{ default: ComponentType }>
+> = {
+  "u-d-s-t-t01": () => import("./UDSTT01"),
+};
